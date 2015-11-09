3a42d5ace69465a4d55a3144c0be9e3cb84885e7
Paul English <paul@onfrst.com>
2015-11-09 10:20:28 -0700

    Add python setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pug',
+      version='0.1',
+      description='Python data and analysis tools',
+      author='Paul English',
+      author_email='paulnglsh@gmail.com',
+      url='https://github.com/log0ymxm/pug',
+      packages=['pug'],
+     )
