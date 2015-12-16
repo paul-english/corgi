6511867e814c8c647bc726dcfc5a97fb8034b672
Paul English <paul@onfrst.com>
2015-12-16 14:42:17 -0700

    Add a listify utility function

--- corgi/iterate.py
+++ corgi/iterate.py
@@ -6,3 +6,9 @@ def groupdict(col, fn, value_fn=list):
     "Performs a groupby on a collection and consumes the generator result into a python dict."
     sorted_col = sorted(col, key=fn)
     return defaultdict(list, {k: value_fn(v) for k, v in groupby(sorted_col, key=fn)})
+
+
+def listify(obj):
+    if not isinstance(obj, list):
+        return [obj]
+    return obj
