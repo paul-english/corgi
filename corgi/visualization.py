6572f323a7cf5121789d510391371051d3014d1b
Paul English <paul@onfrst.com>
2016-08-08 14:19:40 -0600

    Add a function for simple command line stdout histogram over an array

---
+++ corgi/visualization.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def hist(data, width=20):
+    counts, values = np.histogram(data)
+    max_count = counts.max()
+
+    for (count, value) in zip(counts, values):
+        scaled = int(round((count / max_count) * width))
+        print('%5.2f' % value, ':', 'X'*scaled)
