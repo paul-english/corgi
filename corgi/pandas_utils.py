e3369232014adf29f78975ff102f8e3aec51b81a
Paul English <paul@onfrst.com>
2016-08-08 14:18:47 -0600

    Remove pandas sample utils, these are built into pandas

--- corgi/pandas_utils.py
+++ corgi/pandas_utils.py
@@ -10,14 +10,3 @@ def remove_single_value_columns(df):
     drop_cols = df.columns[drop_ix]
     df = df.drop(drop_cols, axis=1)
     return df
-
-
-def sample(df, sample_percent=2e-2):
-    sample_n = math.floor(len(df) * sample_percent)
-    rows = np.random.choice(df.shape[0], sample_n)
-    return df.ix[rows]
-
-
-def sample_columns(df, sample_percent=0.5):
-    df = sample(df.T, sample_percent).T
-    return df
